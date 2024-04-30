fca596ded9537fb79357e03d53ac37aab8a05c56
aleleotta <114069815+aleleotta@users.noreply.github.com>
2024-04-30 11:14:30 +0200

    .

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,7 +1,12 @@
 from multiprocessing import Process
 
-def todo():
-    pass
+def sumaArgumentos(num1, num2):
+    suma = 0
+    if num1 > num2:
+        num1,num2 = num2,num1
+    for i in range(num1, num2+1):
+        suma = suma + i
+    return suma
 
 if __name__ == "__main__":
     pass
